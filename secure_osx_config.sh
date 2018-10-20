0cd5e145323c4d8b998087ba4e04fd7a62a1a0cf
Jason <12900140+jtruppi@users.noreply.github.com>
2018-10-19 21:05:40 -0700

    Added some useful commands
    Added new commands and added additional commenting on command sections.

--- secure_osx_config.sh
+++ secure_osx_config.sh
@@ -53,40 +53,31 @@ PACKAGES=(
     ack
     autoconf
     automake
-    boot2docker
-    ffmpeg
-    gettext
-    gifsicle
+    awk
+    gawk
     git
-    graphviz
+    gimp
     hub
-    imagemagick
     jq
-    libjpeg
-    libmemcached 
-    lynx
-    markdown
-    memcached
-    mercurial
+    nmap
     npm
+    openssl
     pkg-config
-    postgresql
     python
     python3
-    pypy
-    rabbitmq
     rename
-    ssh-copy-id
     terminal-notifier
-    the_silver_searcher
     tmux
     tree
     vim
     wget
+    wireshark
 )
 
 echo "Installing packages..."
 brew install ${PACKAGES[@]}
+
+brew install curl --with-openssl
 
 echo "Cleaning up..."
 brew cleanup
@@ -107,9 +98,7 @@ CASKS=(
     screen
     skype
     slack
-    spectacle
     sublime-text
-    vagrant
     virtualbox
     vlc
 )
@@ -145,26 +134,66 @@ sudo gem install ${RUBY_GEMS[@]}
 echo "Installing global npm packages..."
 npm install marked -g
 
-echo "Configuring OSX to TLDR Security standard..."
+echo "Configuring OSX to General and Security standard..."
 
+####SPEED
+# Disable animations when opening and closing windows
+defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
+
+# Disable animations when opening from Dock
+defaults write com.apple.dock launchanim -bool false
+
+# Disable animation when opening Info inside Finder
+defaults write com.apple.finder DisableAllAnimations -bool true
+
+####POWER
+#Set computer sleep time in minutes
+systemsetup -setcomputersleep 30
+
+#Set display sleep time in minutes
+systemsetup -setdisplaysleep 15
+
+####INPUTS
 # Set fast key repeat rate
 #defaults write NSGlobalDomain KeyRepeat -int 0
 
-# Require password as soon as screensaver or sleep mode starts
-defaults write com.apple.screensaver askForPassword -int 1
-defaults write com.apple.screensaver askForPasswordDelay -int 0
-
-# Show filename extensions by default
-defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+# Disable "natural" scroll
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
 
 # Enable tap-to-click
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
 defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 
-# Disable "natural" scroll
-defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+# Disable crash reporter
+#defaults write com.apple.CrashReporter DialogType none
+
+####UI
+# Show filename extensions by default
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+####SECURITY
+# Require password as soon as screensaver or sleep mode starts
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+# Turn remote login (SSH) OFF or ON
+systemsetup -setremotelogin off
+
+# Enable Gatekeeper for application code signing installs
+spctl --master-enable
+
+
 
 echo "Creating folder structure..."
 [[ ! -d Workspace ]] && mkdir Workspace
 
 echo "Bootstrapping complete"
+
+#------END OF SCRIPT-------
+#MANUAL STEPS THAT REQUIRE ADDITIONAL USER INPUT
+#DO NOT UNCOMMENT THIS SECTION!!!
+
+#Enable Disk Encryption
+#fdesetup enable
+
+
